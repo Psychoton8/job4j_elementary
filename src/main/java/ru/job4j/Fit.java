7e011edb75e001a47882ae1df054cfc460a00548
psychoton8 <arrow1564940milana@gmail.com>
2022-03-01 05:33:05 +0300

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/Fit.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class Fit {
+    public static double manWeight(short height) {
+        double rsl = (height - 100) * 1.15;
+        return rsl;
+    }
+
+    public static double womanWeight(short heightW) {
+        double rsl = (heightW - 110) * 1.15;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        short height = 187;
+        double man = Fit.manWeight(height);
+        System.out.println("Man 187 is " + man);
+        short heightW = 170;
+        double woman = Fit.womanWeight(heightW);
+        System.out.println("Woman 170 is " + woman);
+    }
+}
