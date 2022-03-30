be3a9fd11506cbd934ce7aeb9e8e45ace22c0e4b
psychoton8 <arrow1564940milana@gmail.com>
2022-03-30 14:15:54 +0300

    1. Модульные тесты. [#159435 #282125]

--- src/test/java/ru/job4j/ConverterTest.java
+++ src/test/java/ru/job4j/ConverterTest.java
@@ -15,27 +15,27 @@ public class ConverterTest {
     }
     @Test
     public void whenConvert5000RblThen83dot3333Dlr() {
-        float in1 = 5000;
-        float expected1 = (float) 83.333336;
-        float out1 = Converter.rubleToDollar(in1);
-        float eps1 = 0.0001f;
-        Assert.assertEquals(expected1, out1, eps1);
+        float in = 5000;
+        float expected = (float) 83.333336;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
 
     }
     @Test
     public void whenConvert7352RblThen122dot53333Dlr() {
-        float in2 = 7352;
-        float expected2 = (float) 122.53333;
-        float out2 = Converter.rubleToDollar(in2);
-        float eps2 =0.0001f;
-        Assert.assertEquals(expected2, out2, eps2);
+        float in = 7352;
+        float expected = (float) 122.53333;
+        float out = Converter.rubleToDollar(in);
+        float eps =0.0001f;
+        Assert.assertEquals(expected, out, eps);
     }
     @Test
     public void whenConvert0RblThen0Dlr() {
-        float in3 = 0;
-        float expected3 = 0;
-        float out3 = Converter.rubleToDollar(in3);
-        float eps3 =0.0001f;
-        Assert.assertEquals(expected3, out3, eps3);
+        float in = 0;
+        float expected = 0;
+        float out = Converter.rubleToDollar(in);
+        float eps =0.0001f;
+        Assert.assertEquals(expected, out, eps);
     }
 }
