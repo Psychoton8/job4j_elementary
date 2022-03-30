0f594a6c296a05adef16696467b44febcd42e442
psychoton8 <arrow1564940milana@gmail.com>
2022-03-30 13:33:25 +0300

    1.1. Что такое import [#504773] MF

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -1,7 +1,29 @@
 package ru.job4j;
 
+import static ru.job4j.math.MathFunction.*;
+
 public class Calculator {
+    public static double sumAndMultiply(double first, double second) {
+        return ru.job4j.math.MathFunction.sum(first, second)
+                + ru.job4j.math.MathFunction.multiply(first, second);
+    }
+    public static double sumSubDiv(double first, double second) {
+        return ru.job4j.math.MathFunction.subtraction(first, second)
+                + ru.job4j.math.MathFunction.divide(first, second);
+    }
+    public static double sumAll(double first, double second) {
+        return ru.job4j.math.MathFunction.sum(first, second)
+                + ru.job4j.math.MathFunction.multiply(first, second)
+                + ru.job4j.math.MathFunction.subtraction(first, second)
+                + ru.job4j.math.MathFunction.divide(first, second);
+    }
+
     public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Сумма вычитания и деления: " + sumSubDiv(10, 30));
+        System.out.println("Сумма всех операций: " + sumAll(10, 40));
+
+
         int one = 1;
         int two = 2;
         int six = 6;
