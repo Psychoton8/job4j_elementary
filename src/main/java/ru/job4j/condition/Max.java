92bd29c37c63361ecc6851d33600f392eb3cd167
psychoton8 <arrow1564940milana@gmail.com>
2022-04-03 12:37:03 +0300

    4.2. Тернарное сравнение [#189 #283160]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -3,8 +3,8 @@ package ru.job4j.condition;
 public class Max {
     public static int max(int left, int right) {
 
-        boolean condition = true;
-        int result = condition ? left : right;
+        boolean condition1 = true;
+        int result = condition1 ? left : right;
         System.out.println("Наибольшее число: " + result);
         return result;
 
