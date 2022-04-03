1c747be828b149d7484a1504dd41ef7a1fc33e08
psychoton8 <arrow1564940milana@gmail.com>
2022-04-03 12:33:32 +0300

    4.1.1. Операторы сравнения в String. [#237406 #283096]

--- src/main/java/ru/job4j/condition/Game.java
+++ src/main/java/ru/job4j/condition/Game.java
@@ -1,14 +1,14 @@
 package ru.job4j.condition;
 
 public class Game {
-    public static void menu(String name) {
-        if ("super mario".equals(name)) {
+    public static void menu(String name1) {
+        if ("super mario".equals(name1)) {
             System.out.println("Start - super mario");
         }
-        if ("tanks".equals(name)) {
+        if ("tanks".equals(name1)) {
             System.out.println("Start - tanks");
         }
-        if ("tetris".equals(name)) {
+        if ("tetris".equals(name1)) {
             System.out.println("Start - tetris");
         }
     }
