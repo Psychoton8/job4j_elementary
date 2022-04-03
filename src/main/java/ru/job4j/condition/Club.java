5e576d2b72e83cfa64be2e6972fab6d9c45d71a5
psychoton8 <arrow1564940milana@gmail.com>
2022-04-03 12:42:20 +0300

    4.3.2. Оператор || [#333784 #283182]

--- src/main/java/ru/job4j/condition/Club.java
+++ src/main/java/ru/job4j/condition/Club.java
@@ -1,8 +1,8 @@
 package ru.job4j.condition;
 
 public class Club {
-    public static void permission(boolean hasMoney, boolean beFriend) {
-        if (hasMoney || beFriend) {
+    public static void permission(boolean hasMoney1, boolean beFriend) {
+        if (hasMoney1 || beFriend) {
             System.out.println("I can go to the club.");
         } else {
             System.out.println("I can't.");
