8da3eb1ab58e5f33bff9a1a0d945a1355d126198
psychoton8 <arrow1564940milana@gmail.com>
2022-04-05 11:19:28 +0300

    6.1. Классический поиск перебором. [#33489]

---
+++ src/test/java/ru/job4j/loop/FindLoopTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindLoopTest {
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] data = {5, 4, 3, 2};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHasNot10ThenMinus1() {
+        int[] data = {7, 15, 23, 0, 77, 43};
+        int el = 10;
+        int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHas43Then3 () {
+        int[] data = {0, 7, 43, 14, 3};
+        int el = 43;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 2;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHasNot88ThenMinus1 () {
+        int[] data = {0, 1, 2, 3, 4, 7};
+        int el = 88;
+        int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+}
