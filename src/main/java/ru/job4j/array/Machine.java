085335ff2d2a1e078873295e5895c26a76d8dfa5
psychoton8 <arrow1564940milana@gmail.com>
2022-04-10 10:15:24 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#34741 #286347]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -12,7 +12,7 @@ public class Machine {
             while (money >= coin) {
                 money -= coin;
                 rsl[size] = coin;
-                size ++;
+                size++;
             }
         }
         return Arrays.copyOf(rsl, size);
